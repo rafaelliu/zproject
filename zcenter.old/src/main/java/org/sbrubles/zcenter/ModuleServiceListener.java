95c9a47b2d90e392b06b8d598ab929dce7dc73b7
rafaelliu <rafaelliu@gmail.com>
2013-05-16 11:49:58 -0300

    removido *.old

--- zcenter.old/src/main/java/org/sbrubles/zcenter/ModuleServiceListener.java
+++
@@ -1,7 +0,0 @@
-package org.sbrubles.zcenter;
-public interface ModuleServiceListener {
-
-    public void moduleRegistered(ModuleService source, Module module);
-    
-    public void moduleUnregistered(ModuleService source, Module module);
-}
